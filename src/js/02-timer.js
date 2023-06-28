70018f57c175c63dfa8c631a5933aee7450563de
Chester <vladislavmarchenko999@gmail.com>
2023-06-28 20:54:00 +0300

    commit

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,18 @@
 // 1.Підключити бібліотеку для календарика
-// 
+//2.
+
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
+
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
